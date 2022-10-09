5155a6569f38fcdfb58ccbff05d787ea789d425a
AntonioLoco <antoniolocorotondo23@gmail.com>
2022-10-10 00:16:38 +0200

    Fixed search bar on focus

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
 // DOM
 const headerMenu = document.getElementById("header-menu");
 const hamburgerButton = document.getElementById("hamburger-button");
-
+const searchBar = document.getElementById("search-hamburger");
+const labelSearch = document.querySelector("label");
+console.log(labelSearch);
 
 // Evento Hamburger Menu Open and Close
 hamburgerButton.addEventListener("click", function(){
     headerMenu.classList.toggle("hamburger-menu-open");
 });
+
+searchBar.addEventListener("focus", function(){
+    labelSearch.classList.add("d-none");
+})
+
+searchBar.addEventListener("blur", function(){
+    labelSearch.classList.remove("d-none");
+})
 
